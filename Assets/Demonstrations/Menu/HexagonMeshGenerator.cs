a49a3c635d3d7ad3fe14e3fa47272a7ad262dff5
Ed Ayers <edward.ayers@outlook.com>
2015-07-07 17:01:39 +0100

    Started making menu.
    Idea is to have a globe of hexagons (plus 12 pentagons)

---
+++ Assets/Demonstrations/Menu/HexagonMeshGenerator.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HexagonMeshGenerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    
+    var meshfilter = GetComponent<MeshFilter>() ;
+    if (meshfilter == null)
+    {
+      return;
+    }
+
+    Mesh mesh = new Mesh();
+    Vector3[] vertices = new Vector3[6];
+    Vector3[] normals = new Vector3[6];
+    int[] triangles = new int[]
+      {
+        0,1,2,
+        0,2,5,
+        5,2,3,
+        5,3,4
+      };
+
+    for (int i = 0; i < 6; i++)
+    {
+      float t = (2 * Mathf.PI) * i / 6f;
+      vertices[i] = new Vector3(Mathf.Sin(t), Mathf.Cos(t));
+      normals[i] = new Vector3(0, 0, 1);
+    }
+
+    mesh.vertices = vertices;
+    mesh.normals = normals;
+    mesh.triangles = triangles;
+
+    meshfilter.mesh = mesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Demonstrations/Menu/MenuGenerator.cs
+++ Assets/Demonstrations/Menu/MenuGenerator.cs
@@ -3,10 +3,47 @@ using System.Collections;
 
 public class MenuGenerator : MonoBehaviour {
 
+  public GameObject UIprefab;
+  public int layers = 5;
+
 	// Use this for initialization
 	void Start () {
-	
+    CreateGrid();
 	}
+
+  void CreateGrid()
+  {
+    float d = Mathf.PI / 2 / (layers - 1);
+    //Debug.LogFormat("")
+    for (int i = 1 - layers; i < layers; i++)
+    {
+      var theta = i * d;
+      int abs = System.Math.Abs(i);
+      if (abs == layers - 1)
+      { 
+        //don't bother for now
+      }
+      else
+      {
+        int radius = (abs == layers - 1) ? 1 : (layers - abs - 1) * 6;
+        float dp = 2 * Mathf.PI / radius;
+        Debug.Log(radius);
+        for (int j = 0; j < radius; j++)
+        {
+          var phi = j * dp;
+          var pos = new Vector3(
+            Mathf.Sin(phi) * Mathf.Cos(theta),
+            Mathf.Sin(theta),
+            Mathf.Cos(phi) * Mathf.Cos(theta));
+          pos *= 5;
+          var go = (GameObject)Instantiate(UIprefab, pos, Quaternion.LookRotation(pos));
+          go.transform.SetParent(this.transform);
+          //Debug.Log("made it ");
+        }
+      }
+    }
+
+  }
 	
 	// Update is called once per frame
 	void Update () {
