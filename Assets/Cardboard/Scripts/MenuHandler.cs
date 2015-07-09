a5145944ecbc740fac8ceed049e38b8503ff292d
Ed Ayers <edward.ayers@outlook.com>
2015-07-09 16:11:59 +0100

    Added some UI to appear when the user is close to going back.

--- Assets/Cardboard/Scripts/MenuHandler.cs
+++ Assets/Cardboard/Scripts/MenuHandler.cs
@@ -1,25 +1,66 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 ///Code to handle going back when in a demo.
 public class MenuHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+    canvas = GameObject.Find("BackUICanvas").GetComponent<Canvas>();
+    canvas.enabled = true;
+    backImage = GameObject.Find("BackImage").GetComponent<RawImage>();
+    backTargetImage = GameObject.Find("BackTargetImage").GetComponent<RawImage>();
+    backImage.enabled = false;
+    backTargetImage.enabled = false;
+    Color.TryParseHexString("44AA44FF", out isaacGreen);
+    ringColor = isaacGreen;
 	}
 
+  Canvas canvas;
+  RawImage backImage;
+  RawImage backTargetImage;
+  Color isaacGreen;
+  Color ringColor;
   float startTime;
   const float TIME_THRESHOLD = 0.1f;
   const float POS_THRESHOLD = 0.2f;
   bool timing = false;
 
+  public Vector3 fakeAcc;
+
 	// Update is called once per frame
 	void Update () {
     if (Application.loadedLevel != 0)
     {
-      if ((Input.acceleration - Vector3.right).magnitude < POS_THRESHOLD)
+#if UNITY_EDITOR
+      var acc = fakeAcc.normalized;
+#else
+      var acc = Input.acceleration;
+#endif
+      float dist = (acc - Vector3.right).magnitude;
+      if (dist < 1)
       {
+        canvas.enabled = true;
+        backImage.enabled = true;
+        backTargetImage.enabled = true;
+        backTargetImage.color = new Color(
+          ringColor.r, 
+          ringColor.g, 
+          ringColor.b, 
+          Mathf.Lerp(0, 1, (1 - dist) * 2));
+        backImage.color = new Color(1, 1, 1, Mathf.Lerp(0, 1, (1 - dist) * 2));
+        backImage.transform.localPosition = new Vector3(acc.y, acc.z,0) * 5;
+
+      }
+      else
+      {
+        canvas.enabled = false;
+      }
+
+      if ((acc - Vector3.right).magnitude < POS_THRESHOLD)
+      {
+        ringColor = new Color(1, 0, 0, 1);
         if (!timing)
         {
           startTime = Time.time;
@@ -37,8 +78,13 @@ public class MenuHandler : MonoBehaviour {
       }
       else if (timing)
       {
+        ringColor = isaacGreen;
         timing = false;
       }
+    }
+    else
+    {
+      canvas.enabled = false;
     }
 	}
 }
