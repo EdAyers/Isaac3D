a307e008384cae766f960f782fbfc29fa5fc1d50
Ed Ayers <edward.ayers@outlook.com>
2015-07-09 11:24:33 +0100

    backup before itween

--- Assets/Demonstrations/Menu/MenuBehaviour.cs
+++ Assets/Demonstrations/Menu/MenuBehaviour.cs
@@ -2,37 +2,51 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class MenuBehaviour : MonoBehaviour {
+public class MenuBehaviour : MonoBehaviour
+{
 
   public int index = 0;
   public int levelNo = 0;
   Canvas canvas;
-  
-  Cardboard c;
 
-	// Use this for initialization
-	void Start () {
-	  //rotate the canvas to be pointing up
+  Cardboard cardboard;
+  Collider collider;
+  Camera camera;
+
+  // Use this for initialization
+  void Start()
+  {
+    //rotate the canvas to be pointing up
     //canvas = this.GetComponentInChildren<Canvas>();
     //canvas.transform.LookAt(transform.position * 3, new Vector3(0, 1, 0));
-    c = GameObject.Find("CardboardMain").GetComponent<Cardboard>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	  if (c.CardboardTriggered)
+    cardboard = GameObject.Find("CardboardMain").GetComponent<Cardboard>();
+    camera = cardboard.GetComponentInChildren<Camera>();
+    collider = this.GetComponentInChildren<MeshCollider>();
+  }
+
+  bool isSelected = false;
+
+  // Update is called once per frame
+  void Update()
+  {
+
+    RaycastHit hit;
+    if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, 500))
     {
-      RaycastHit hit;
-      var cam = c.GetComponentInChildren<Camera>();
-      if(Physics.Raycast (cam.transform.position, cam.transform.forward, out hit, 500))
+      if (hit.collider == collider)
       {
-        Debug.Log("raycast hit");
-        if (hit.collider == this.GetComponentInChildren<MeshCollider>())
+        if (cardboard.CardboardTriggered)
         {
-          GetComponentInChildren<Text>().text = "LOADING...";
-          Application.LoadLevel(levelNo);
+            GetComponentInChildren<Text>().text = "LOADING...";
+            Application.LoadLevel(levelNo);
+        }
+        else
+        {
+          isSelected = true;
         }
       }
+
     }
-	}
+
+  }
 }
